5e49e323d057f874d060a486bf8b2ab5cd70f451
Cashayme <53022082+Cashayme@users.noreply.github.com>
2025-09-04 02:51:11 +0200

    Modification de la configuration TypeScript : désactivation de l'option "strict" et ajout de "noEmitOnError" dans tsconfig.json.

---
+++ global.d.ts
@@ -0,0 +1,9 @@
+declare var process: any;
+
+declare module "express";
+declare module "cors";
+declare module "morgan";
+declare module "express-session";
+declare module "path";
+declare module "fs";
+declare module "url";
